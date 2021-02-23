ba15915119b716550b782eb08012814176990857
Bibin Nahas <bbnnaz@gmail.com>
2021-02-23 07:51:01 +0530

    modified twitter code to save as file

--- src/main/scala/com/me/twitter/entry/PrintTweets.scala
+++ src/main/scala/com/me/twitter/entry/PrintTweets.scala
@@ -12,14 +12,28 @@ object PrintTweets {
 
     // Configure Twitter credentials using twitter.txt
     setupTwitter()
-    val ssc = new StreamingContext("local[*]", "PrintTweets", Seconds(3))
+    val ssc = new StreamingContext("local[*]", "PrintTweets", Seconds(1))
 
     // Get rid of log spam (should be called after the context is set up)
     setupLogging()
 
     val tweets = TwitterUtils.createStream(ssc, None)
     val statuses = tweets.map(status => status.getText())
-    statuses.print()
+
+    //    print tweets
+    //    statuses.print()
+
+    var totalTweets: Long = 0
+    statuses.foreachRDD((rdd, time) => {
+      if (rdd.count > 0) {
+        val repartitionRdd = rdd.coalesce(1).cache()
+        repartitionRdd.saveAsTextFile(s"Tweets_${time.milliseconds.toString}")
+        totalTweets += repartitionRdd.count()
+        println(s"Toatal tweets so far: $totalTweets")
+        if (totalTweets > 1000) System.exit(0)
+      }
+    })
+
     ssc.start()
     ssc.awaitTermination()
   }
